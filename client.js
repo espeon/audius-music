53672d01b076bb4e502e4eb8768bb2e24d699db9
linustek <brocklix@icloud.com>
2019-12-15 16:16:07 -0600

    switch to dotenv

--- client.js
+++ client.js
@@ -1,183 +1,133 @@
-const Discord = require('discord.js')
-const YouTube = require(`simple-youtube-api`)
-const ytdl = require(`ytdl-core`)
-const ydl = require(`youtube-dl`)
-const fs = require('fs') // For reading command files - Bass
-let m // Message placeholder - Bass
-const bot = new Discord.Client({ disableEveryone: true })
-bot.commands = new Discord.Collection()
-const { token, prefix, ytkey, sckey } = require('./config.json')
-const youtube = new YouTube(ytkey)
+require('dotenv').config()
+const Discord = require("discord.js");
+const YouTube = require("simple-youtube-api");
+const ytdl = require("ytdl-core");
+const ydl = require("youtube-dl");
+const fs = require("fs"); // For reading command files - Bass
+global.message; // Message placeholder - Bass
+const client = new Discord.Client({ disableEveryone: true });
+client.commands = new Discord.Collection();
+const { token, prefix, ytkey, sckey } = process.env;
+const youtube = new YouTube(ytkey);
 
-const commandFiles = fs.readdirSync('./commands') // read the folder that holds the commands, 
-                                                  // and blocks all other code from running until it completes
-for (const file of commandFiles) {                // for each file in commandFiles...
-  const command = require(`./commands/${file}`)   // ...require it...
-  bot.commands.set(command.name, command)         // ...and add to list of commands
+const commandFiles = fs.readdirSync("./commands"); // read the folder that holds the commands,
+// and blocks all other code from running until it completes
+for (const file of commandFiles) {
+  // for each file in commandFiles...
+  const command = require(`./commands/${file}`); // ...require it...
+  client.commands.set(command.name, command); // ...and add to list of commands
 }
 
-let id = `id-001`
-let title = `NGGYU`
-let murl = `https://youtube.com`
-let streamlink = `https://audius.co/this-is/a-link-1332`
-let bitrate = 320
+let id = "id-001";
+let title = "NGGYU";
+let murl = "https://youtube.com";
+let streamlink = "https://audius.co/this-is/a-link-1332";
+let bitrate = 320;
 
-const queue = new Map()
+global.queue = new Map();
 
-bot.on(`warn`, console.warn)
-bot.on(`error`, console.error)
+client.on("warn", console.warn);
 
-bot.on(`ready`, () => {
-  console.log(`Online and ready to DJ! Prefix is ${prefix}`)
-})
+client.on("error", console.error);
 
-bot.on(`voiceStateUpdate`, (n, member) => { // Makes sure the bot leaves the vc
-  let vc = member.voiceChannel
+client.on("ready", () => {
+  console.log(`Online and ready to DJ! Prefix is ${prefix}`);
+  client.user.setActivity("some hot tunes", { type: "LISTENING" });
+});
 
-  if (vc.members.array().length == 0) {
-    vc.leave()
-  }
-})
-bot.on(`message`, async msg => {
-  m = msg // Used for errors
+client.on("message", async msg => {
+  global.message = msg; // Used for errors
 
   // eslint-disable-line
   if (!msg.content.startsWith(prefix) || msg.author.bot) {
-    return // No need for `return undefined` - Bass
+    return; // No need for `return undefined` - Bass
   }
-  // if (msg.channel.type === `dm`) return undefined
+  // if (msg.channel.type === "dm") return undefined
   // Might have no need for this, my bot doesn't respond to DMs anyways - Bass
 
-  function parseArgs (argString, argCount, allowSingleQuote = true) {
+  function parseArgs(argString, argCount, allowSingleQuote = true) {
     // Replace smart quotes (i.e. “ ” ‘ ’ ) with straight double quotes " "
-    argString = argString.replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"')
-    const re = allowSingleQuote ? /\s*(?:("|')([^]*?)\1|(\S+))\s*/g : /\s*(?:(")([^]*?)"|(\S+))\s*/g
-    const result = []
-    let match = []
+    argString = argString
+      .replace(/[\u2018\u2019]/g, "'")
+      .replace(/[\u201C\u201D]/g, '"')
+      .replace(/>/g, "")
+      .replace(/</g, "");
+    const re = allowSingleQuote
+      ? /\s*(?:("|')([^]*?)\1|(\S+))\s*/g
+      : /\s*(?:(")([^]*?)"|(\S+))\s*/g;
+    const result = [];
+    let match = [];
     // Large enough to get all items
-    argCount = argCount || argString.length
+    argCount = argCount || argString.length;
     // Get match and push the capture group that is not null to the result
-    while (--argCount && (match = re.exec(argString))) result.push(match[2] || match[3])
+    while (--argCount && (match = re.exec(argString)))
+      result.push(match[2] || match[3]);
     // If text remains, push it to the array as-is (except for wrapping quotes, which are removed)
     if (match && re.lastIndex < argString.length) {
-      const re2 = allowSingleQuote ? /^("|')([^]*)\1$/g : /^(")([^]*)"$/g
-      result.push(argString.substr(re.lastIndex).replace(re2, '$2'))
+      const re2 = allowSingleQuote ? /^("|')([^]*)\1$/g : /^(")([^]*)"$/g;
+      result.push(argString.substr(re.lastIndex).replace(re2, "$2"));
     }
-    return result
+    return result;
   }
 
-  const args = parseArgs(msg.content.slice(prefix.length)) // slices the prefix from the command
-  const commandName = args.shift().toLowerCase() /* make the command lowercase 
+  const args = parseArgs(msg.content.slice(prefix.length)); // slices the prefix from the command
+  const command = args
+    .shift()
+    .toLowerCase(); /* make the command lowercase 
                                                 EXAMPLE: let string = 'ArE yoU MoCkInG Me?'
                                                 let lowerString = string.shift().toLowerCase()
                                                 // lowerString is 'are you mocking me?' 
                                                 - Bass
                                                 */
-  const searchString = args.join(` `)
-  const url = args[1] ? args[1].replace(/<(.+)>/g, `$1`) : ``
-  const serverQueue = queue.get(msg.guild.id)
+  const searchString = args.join(" ");
+  const url = args[1] ? args[1].replace(/<(.+)>/g, "$1") : "";
+  const serverQueue = global.queue.get(msg.guild.id);
 
-  const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
-
-  if (!command) { // if the bot doesnt have the command specified...
-    msg.reply(`I don't have that command, did you mean to use another bot?`) // ...tell the user...
-    return // ...and exit - Bass
+  const cmd =
+    client.commands.get(command) ||
+    client.commands.find(
+      cmdo => cmdo.aliases && cmdo.aliases.includes(command)
+    );
+  if (cmd === "restart"){
+    if (msg.author.id == 267121875765821440) {
+      msg.channel.send("Restarting music bot...");
+      client.destroy();
+      process.exit(1);
+    }
+    return msg.channel.send("You cannot use this command.");
   }
+  if (cmd == undefined) return;
+  console.log(cmd);
   try {
-    command.execute(bot, msg, args, serverQueue, youtube, queue)
+    cmd.execute(client, msg, args, serverQueue, youtube);
+  } catch (error) {
+    console.error(error);
+    msg.channel.send("there was an error trying to execute that command!");
   }
-  // The try block above tries to execute a command. If it fails for whatever reason, it throws a error that the 
-  // catch block below grabs (thats why its called a "catch" block) and does something with the error. - Bass
-  catch (error) {
-    console.error(error)
-    msg.reply(`There was an error trying to execute that command! \nError: \`\`\`css\n' ${Error(error)}\`\`\``)
-  }
-})
+  
+  return undefined;
+});
 
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id)
-
-  if (!song) {
-    serverQueue.voiceChannel.leave()
-    serverQueue.textChannel.send(
-      `i've left as there's nothing in queue. to add me back in, just queue something up!`
-    )
-    queue.delete(guild.id)
-    bot.user.setActivity(`some hot tunes`, { type: `LISTENING` })
-    return
-  }
-  console.log(serverQueue.songs)
-  console.log(song.url)
-  if (song.url.includes(`youtube`)) {
-    const dispatcher = serverQueue.connection
-      .play(ytdl(song.url), {
-        volume: 0.5,
-        bitrate: serverQueue.bitrate,
-        passes: 10
-      })
-      .on(`end`, reason => {
-        if (reason === `Stream is not generating quickly enough.`)
-          console.log(`Song ended.`)
-        else console.log(reason)
-        serverQueue.songs.shift()
-        play(guild, serverQueue.songs[0])
-      })
-      .on(`error`, error => console.error(error))
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 100)
-  } else if (song.url.includes(`sndcdn`)) {
-    const dispatcher = serverQueue.connection
-      .play(song.url, {
-        volume: 0.5,
-        bitrate: serverQueue.bitrate,
-        passes: 10
-      })
-      .on(`end`, reason => {
-        if (reason === `Stream is not generating quickly enough.`)
-          console.log(`Song ended.`)
-        else console.log(reason)
-        serverQueue.songs.shift()
-        play(guild, serverQueue.songs[0])
-      })
-      .on(`error`, error => console.error(error))
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 100)
-  } else if (song.url.includes(`audius`)) {
-    const dispatcher = serverQueue.connection
-      .play(song.url, {
-        volume: 0.5,
-        bitrate: serverQueue.bitrate,
-        passes: 10
-      })
-      .on(`end`, reason => {
-        if (reason === `Stream is not generating quickly enough.`)
-          console.log(`Song ended.`)
-        else console.log(reason)
-        serverQueue.songs.shift()
-        play(guild, serverQueue.songs[0])
-      })
-      .on(`error`, error => console.error(error))
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 100)
-  }
-  serverQueue.textChannel.send(`Now playing **${song.title}**`)
-}
-
-process.on('unhandledRejection', function(error) {
-  console.error(`Uncaught Promise Rejection:\n${error}`)
-  m.reply(`Uncaught Promise Rejection:\n${error}`)
-})
+process.on("unhandledRejection", function(error) {
+  console.error(`Uncaught Promise Rejection:\n${error}`);
+  global.message.reply(`Uncaught Promise Rejection:\n${error}`);
+});
 // ^ Promise rejections are confusing, heres a link: https://davidwalsh.name/promises - Bass
 
-process.on('uncaughtException', function (err) {
-  console.log(`Caught exception: ${err}`)
-  //m.reply(`Fatal error, please alert an Audius staff member about this!`)
-  bot.destroy()
-})
+process.on("uncaughtException", function(err) {
+  console.log(`Caught exception: ${err}`);
+  global.message.reply(
+    `Fatal error, please alert an Audius staff member about this!`
+  );
+  client.destroy();
+});
 // ^ These are dangerous, if yall want yall can add logging to files so yall can see what went wrong
 
 // Kills the bot when it recieves a SIGINT, Control+C - Bass
 
-/*process.on('SIGINT', () => {
-  console.log('Stopping bot.')
-  bot.destroy()
-})*/
+process.on("SIGINT", () => {
+  console.log("Stopping bot.");
+  client.destroy();
+});
 
-bot.login(token)
+client.login(token);
--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { token } = process.env;
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./client.js', { token: token });
